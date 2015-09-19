5c4abdfc2c99992533f33ae1db302e2bbb2bb5ba
Stefanorn <stefanorn92@gmail.com>
2015-09-19 16:19:51 +0000

    Main menu
    Gerði leik ekki endalausan,
    main menu
    og fade

---
+++ Drinking Games/Assets/MainMenuScrip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScrip : MonoBehaviour
+{
+    public Image curtains;
+
+    float cuirtainAlphaFader = 0f;
+
+    public void FUBAR()
+    {
+        Application.LoadLevel(1);
+    }
+    void OnLevelWasLoaded()
+    {
+        curtains.gameObject.SetActive(true);
+    }
+    void Update()
+    {
+        curtains.color = new Color(255, 255, 255, cuirtainAlphaFader);
+        cuirtainAlphaFader = Mathf.Lerp(1, 0, Time.time *2 );
+        if (cuirtainAlphaFader <= 0)
+        {
+            curtains.gameObject.SetActive(false);
+        }
+
+    }
+}
--- Drinking Games/Assets/gameLogic.cs
+++ Drinking Games/Assets/gameLogic.cs
@@ -17,20 +17,50 @@ public class gameLogic : MonoBehaviour {
     GameObject cardChecker;
     public Text  rule;
     public Image cardImage;
+    public Image curtains;
     public Sprite backCover;
     public float rotateTimer = 1f;
     public Quaternion startRotation;
 
-	// Use this for initialization
-	void Start () {
+    int[] chooesnCardIndex = new int[150]; ///HARÐKÓÐUN!!! Þarf að gera grein fyrir deck.length
+    int cardCounter = 0;
+    float cuirtainAlphaFader = 0;
+    float levelChangeTimer = 2;
+    float fadeTimer = 0; 
+
+    // Use this for initialization
+    void Start () {
         cardChecker = GameObject.FindGameObjectWithTag("Card");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (cardChecker.active == false)
+        if (cardChecker.active == false) //This code only runs one per frame
         {
+            if (cardCounter == 4)
+            {
+                
+                curtains.gameObject.SetActive (true);
+                curtains.color = new Color(255, 255, 255, cuirtainAlphaFader);
+                cuirtainAlphaFader = Mathf.Lerp(0, 1, fadeTimer / levelChangeTimer);
+                fadeTimer += Time.deltaTime;
+                if (!IsInvoking("BackToMainMenu"))
+                {
+                    Invoke("BackToMainMenu", 2);
+                }
+                return;
+            }
+
             int randomNumber = Random.Range(0, deck.Length - 1);
+            foreach (int index in chooesnCardIndex) //Check if the card have been drawn before
+            {
+                if (index == randomNumber)
+                {
+                    randomNumber = Random.Range(0, deck.Length - 1); ///BUG? gæti verið að random gefi sömu tölu aftur
+                }
+            }
+            chooesnCardIndex[cardCounter] = randomNumber;
+            cardCounter++;
 
             cardImage.sprite = backCover;
             cardImage.tag = "Card";
@@ -41,6 +71,10 @@ public class gameLogic : MonoBehaviour {
         }
 	
 	}
+    void BackToMainMenu()
+    {
+        Application.LoadLevel(0);
+    }
     IEnumerator RotateCard(Sprite drawnCard)
     {
        float timer = 0;
@@ -48,7 +82,6 @@ public class gameLogic : MonoBehaviour {
         while(timer < rotateTimer)
         {
             cardChecker.transform.rotation = Quaternion.Lerp(orgRotation, Quaternion.Euler(0,0,0), timer/rotateTimer );
-            Debug.Log(timer / rotateTimer);
             if(timer / rotateTimer >= 0.5)
             {
                 cardImage.sprite = drawnCard;

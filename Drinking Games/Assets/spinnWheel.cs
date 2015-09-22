962cdd43a14f7782db51aa71acb2fa9363836538
Stefanorn <stefanorn92@gmail.com>
2015-09-22 21:37:40 +0000

    Lukkuhjól
    Bætti inn lukkuhjóli,grafík , heimskur kóði sem snýr hjólinu þarf að
    vinna í

---
+++ Drinking Games/Assets/spinnWheel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinnWheel : MonoBehaviour {
+
+    Vector3 mouseClickPosition;
+    Vector3 mouesEndClickPosition;
+    float dragDist;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown()
+    {
+       mouseClickPosition = Input.mousePosition;
+    }
+    void OnMouseUp()
+    {
+        mouesEndClickPosition = Input.mousePosition;
+    }
+    void FixedUpdate()
+    {
+        dragDist = Vector3.Distance(mouesEndClickPosition, mouseClickPosition);
+        Camera.main.transform.Rotate(0, 0, dragDist * Time.deltaTime);
+    }
+
+}
